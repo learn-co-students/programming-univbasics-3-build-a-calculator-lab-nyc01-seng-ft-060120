29471c4cf463af5fafda2d8e77a0b0c3cac2385a
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-10 16:01:00 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  addition_result = num1+num2
+  addition_result
 end
 
 def subtraction(num1, num2)
-
+  subtraction_result = num1-num2
+  subtraction_result
 end
 
 def division(num1, num2)
-
+  division_result = num1/num2
+  division_result
 end
 
 def multiplication(num1, num2)
-
+  multiplication_result = num1*num2
+  multiplication_result
 end
 
 def modulo(num1, num2)
-
+  modulo_result = num1%num2
+  modulo_result
 end
 
 def square_root(num)
-
+  Math.sqrt(num)
 end
